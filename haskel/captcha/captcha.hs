e3aa215e366023697907beac8a6606e85605aee5
Muhanad Abdalla <muhanad.abdalla@aofl.com>
2018-01-04 23:56:42 -0800

    added sol2

--- haskel/captcha/captcha.hs
+++ haskel/captcha/captcha.hs
@@ -3,14 +3,15 @@ import System.Environment
 -- A function to convert string of numbers into an array of integers
 digitize x = map (read . (:"")) x :: [Int]
 
-
 -- Create a new list with that unshift every element by 1 pos and put the 1st element in the end.
-tuppleIt a = zip a ( tail a ++ take 1 a)
+sol1 a = zip a ( tail a ++ take 1 a)
 
+sol2 a = let len = length a `div` 2
+   in zip (take len a) (reverse $ take len $ reverse a)
 isEqual x =  if fst x == snd x then True else False
 
-captcha x =  sum $ map fst $ filter isEqual ( tuppleIt (digitize x))
+captcha1 x =  sum $ map fst $ filter isEqual ( sol1 (digitize x))
+captcha2 x =  sum $ map fst $ filter isEqual ( sol2 (digitize x))
 --captchaSum x = a <- (prepInput x)
 main = do x <- getArgs
-          print (captcha $ head x)
-
+          print (captcha2 $ head x)
